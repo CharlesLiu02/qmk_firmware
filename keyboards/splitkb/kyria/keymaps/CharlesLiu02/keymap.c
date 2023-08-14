2c38848021caeb07eadd5308f53a543f8917d42e
CharlesLiu02 <liu.charles02@gmail.com>
2023-08-14 00:43:45 -0700

    fix base layer

--- keyboards/splitkb/kyria/keymaps/CharlesLiu02/keymap.c
+++ keyboards/splitkb/kyria/keymaps/CharlesLiu02/keymap.c
@@ -45,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q , KC_W   , KC_E   , KC_R   , KC_T   ,                                                  KC_Y   , KC_U   , KC_I  , KC_O   , KC_P   , KC_CAPS,
+     KC_TAB  , KC_Q , KC_W   , KC_E   , KC_R   , KC_T   ,                                                  KC_Y   , KC_U   , KC_I  , KC_O   , KC_P   , KC_MINS,
      KC_ESC  , KC_A , KC_S   , KC_D   , KC_F   , KC_G   ,                                                  KC_H   , KC_J   , KC_K  , KC_L   , KC_SCLN, KC_QUOT,
-     KC_LSFT , KC_Z , KC_X   , KC_C   , KC_V   , KC_B   , KC_LCTL, FKEYS_NAV,   KC_RALT, KC_RCTL, KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                               KC_MUTE, KC_LGUI, KC_LALT, KC_SPC , KC_LCTL  ,   KC_ENT , NUM    , KC_BSPC, KC_RGUI, KC_MUTE
+     KC_LSFT , KC_Z , KC_X   , KC_C   , KC_V   , KC_B   , KC_EQL , FKEYS_NAV,            KC_RALT, KC_RCTL, KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                               KC_CAPS, KC_LGUI, KC_LALT, KC_SPC , KC_LCTL  ,            KC_ENT , NUM    , KC_BSPC, KC_RGUI, KC_MUTE
     ),
 
 /*
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM] = LAYOUT(
       _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
       _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
-      _______, XXXXXXX, KC_TILD, KC_GRV , KC_LBRC, KC_LCBR, _______, _______, _______, _______, KC_RCBR, KC_RBRC, _______, _______, _______, _______,
+      _______, XXXXXXX, KC_TILD, KC_GRV , KC_LBRC, KC_LCBR, _______, _______, _______, _______, KC_RCBR, KC_RBRC, _______, _______, _______, KC_BSLS,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
